44b3068dd6f3846e754b4f6afbc69018436204a6
lopezrunco <lopezrunco@gmail.com>
2021-11-24 12:12:40 -0300

    Code cleaned and favicon

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-var inputPassword = document.getElementById("password")     // Obtiene el input #password donde se mostrara la contrasena generada
-var messageElement = document.getElementById("message")     // Obtiene el elemento de mensaje de contrasena copiada
-var lengthElement = document.getElementById("length")       // Obiente el elemento input donde se ingresa el largo de la contrasena
+var inputPassword = document.getElementById("password")
+var messageElement = document.getElementById("message")
+var lengthElement = document.getElementById("length")
 
-// Funcion que genera una contrasena aleatoria
 function generatePassword() {
-    var characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+?><:{}[]"   // Caracteres a usar
-    var passwordLenght = lengthElement.value        // Largo de la contrasena
-    var password = ""                               // Variable donde se guardara la contrasena
+    // Characters to use
+    var characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+?><:{}[]"
 
-    // Itera por el largo de la contrasena y da un valor aleatorio
-    for (let charactersIndex = 0; charactersIndex < passwordLenght; charactersIndex++) {    
-        var randomNumber = Math.floor(Math.random() * characters.length)    // Genera un numero aleatorio y lo guarda en una variable
-        password += characters.substring(randomNumber, randomNumber + 1)    // Substring extrae caracteres de la variable characters, usando como indice la variable randonNumber y randonNumber mas 1
-                                                                            // con el simbolo += los guardamos en la variable password uno detras del otro
+    var passwordLenght = lengthElement.value
+
+    // Variable when the password will be saved
+    var password = ""
+
+    for (let charactersIndex = 0; charactersIndex < passwordLenght; charactersIndex++) {
+
+        // Generates a random number and save it o a variable
+        var randomNumber = Math.floor(Math.random() * characters.length)
+
+        // Substring extracts charaters from the characters variables, using randomNumber as an index and randonNumber plus 1
+        password += characters.substring(randomNumber, randomNumber + 1)
     }
-    inputPassword.value = password  // Asigna la contrasena generada a el input password
+
+    // Shows the password on the input
+    inputPassword.value = password
     
-    messageElement.classList.add("d-none")  // Al generar una contrasena nueva, se oculta el mensaje de copiado
+    // On password generated, hides copied message
+    messageElement.classList.add("d-none")
 }
 
-// Funcion para copiar contrasena al portapapeles
-function myFunction() {
-    inputPassword.select()                      // Selecciona el campo de texto
-    inputPassword.setSelectionRange(0, 99999)   // Selecciona el campo en mobiles
-
-    document.execCommand("copy")                // El metodo execCommand ejecuta un comando especifico (en este caso "copy") en una seccion editable
-
-    // alert(inputPassword.value + " has been copied to clipboard")    // Aviso de que se ha copiado la contrasena en alert
-
-    messageElement.classList.remove("d-none")   // Muestra el aviso de copiado a la contrasena en el document
+function copyToClipBoard() {
+    inputPassword.select()
+    inputPassword.setSelectionRange(0, 99999)
+    document.execCommand("copy")
+    messageElement.classList.remove("d-none")
 }
